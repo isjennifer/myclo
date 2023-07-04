828fbcda084432f1306803225ad1a1c38c0a0b8e
isjennifer <duensdl@gmail.com>
2023-07-04 22:34:29 +0900

    user 페이지 골조 완성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -36,6 +36,7 @@ const Button = styled.button`
     font-size: 18px;
     font-weight: 600;
     border: none;
+    color: #505050;
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
     &:hover{
         cursor: pointer;
--- src/routes/User.js
+++ src/routes/User.js
@@ -10,23 +10,35 @@ const BackgroundImg = styled.div`
     background: url('${background_img}') no-repeat;
     background-size: cover;
     z-index: 1;
+    overflow: hidden;
 
 `
 const Circle = styled.div`
+    display: flex;
+    flex-direction: column;
     position: absolute;
+    padding: 45vh 30vh 45vh 0vh;
     width: 1000px;
     height: 1000px;
+    justify-content: space-evenly;
+
+    align-items: center;
     background-color: rgba(255, 255, 255, 0.5);
     border-radius: 100%;
-    top: -100px;
-    left: 800px;
+    top: -13%;
+    left: 55%;
     z-index: 2;
-
+    color: #505050;
+    letter-spacing: 2px;
+    font-size: 30px;
+    font-weight: 700;
 `
 const Letter = styled.div`
     display: flex;
+    position: relative;
+    top: 35vh;
+    left: 35vh;
     height: 250px;
-    margin-top: 200px;
     justify-content: space-evenly;
     align-items: center;
     flex-direction: column;
@@ -46,6 +58,7 @@ const Button = styled.button`
     font-size: 18px;
     font-weight: 600;
     border: none;
+    color: #505050;
     box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
     &:hover{
         cursor: pointer;
@@ -56,7 +69,11 @@ function Home() {
   return (
     <>
     <BackgroundImg>
-        <Circle />
+        <Circle>
+            <span>반갑습니다, user 님!</span>
+            <span>오늘의 날씨는 맑음 입니다.</span>
+            <span>현재 기온은 14.8도 입니다.</span>
+        </Circle>
         <Navbar />
         <div style={{display: "inline-flex"}}>
             <Letter>
@@ -64,9 +81,10 @@ function Home() {
                     <Button>내 옷장에서 옷 고르기</Button>
                     <Button>사람들에게 추천받기</Button>
             </Letter>
-            
+            <Letter>
+
+            </Letter>
         </div>
-        
     </BackgroundImg>
 
     </>
